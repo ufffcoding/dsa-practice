a6ad9a7585e6c19dbf78189a5f866c7940cbcf38
Deepanshuverma00 <deepanshusv3@gmail.com>
2024-06-21 01:34:56 +0530

    reverse string

---
+++ reverseWords.js
@@ -0,0 +1,18 @@
+// function reverseWords(s) {
+//   return s
+//     .split(" ")
+//     .slice()
+//     .filter((i) => i !== "")
+//     .reverse()
+//     .join(" ");
+// }
+
+// const res = reverseWords("hey there  am good");
+// console.log(res);
+
+function reverseWords(s) {
+  return [...s]
+}
+
+const res = reverseWords("hey there  am good");
+console.log(res);
